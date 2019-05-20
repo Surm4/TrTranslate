ee1340585499ce42c55d86736d0e3e853f7c472f
SurmaAa <anondebilwkazdymfredzie@gmail.com>
2019-05-20 22:50:49 +0200

    changed textares to div content editable

--- public/js/textarea-control.js
+++ public/js/textarea-control.js
@@ -3,33 +3,11 @@ const TextAreaControl = () => {
     const documentTranslation = document.querySelector("#documentTranslation");
     documentToTranslate.addEventListener("keyup", _.debounce((e) => { handleTyping(e) }, 500));
 
-   
     const initialTextareaHeight = documentToTranslate.scrollHeight;
     let oldTextareaHeight = 0;
     const handleTyping = (e) => {
-        const currentTextareaHeight = e.target.scrollHeight;
-        shouldChangeHeight(currentTextareaHeight);
+        documentTranslation.style.height = `${documentToTranslate.clientHeight}px`;
     };
-
-    const shouldChangeHeight = (currentTextareaHeight) => {
-        const documentToTranslateIsNotEmpty = documentToTranslate.value.trim();
-        console.log(currentTextareaHeight);
-        if (documentToTranslateIsNotEmpty) {
-            if (currentTextareaHeight !== oldTextareaHeight && currentTextareaHeight > initialTextareaHeight && currentTextareaHeight > oldTextareaHeight) { 
-            } else {
-               // i think instrad of calculating it will be changed to content editable div and it will be faster
-            }
-        } else {
-            setNewHeight(initialTextareaHeight);
-        }
-    };
-
-    const setNewHeight = (height) => {
-        oldTextareaHeight = height;
-        documentToTranslate.style.height = `${height}px`;
-        documentTranslation.style.height =  `${height}px`;
-    };
-
 };
 
 export default TextAreaControl;
