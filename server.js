b076c9ec7a04d63c5570bc59593ff0d8be03f5df
SurmaAa <anondebilwkazdymfredzie@gmail.com>
2019-06-03 23:55:26 +0200

    removing harcode from server
    #wait for import() to remove hardcoded require

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
 const ServerControl = (() => {
     const translationControl = require(`./translation.js`).TranslationControl;
+    const configuration = require("./public/config/config.json");
     const express = require("express");
     const app = express();
     const port = 80;
 
-    app.listen(port, () => console.log(`Server started`));
-    app.use(express.static(`${__dirname}/public`));
+    app.listen(port, () => console.log(`${configuration.msg.dev.log.serverStarted}`));
+    app.use(express.static(`${__dirname}${configuration.folders.client.public}`));
 
-    app.get('/', (req, res) => res.sendFile(`${__dirname}/public/www/index.html`));
+    app.get(`${configuration.file.server.req.indexWWW}`, (req, res) => res.sendFile(`${__dirname}${configuration.file.server.res.indexWWW}`));
 
-    app.get('/list-control', (req, res) => res.sendFile(`${__dirname}/public/js/list-control.js`));
-    app.get('/navigation-ui', (req, res) => res.sendFile(`${__dirname}/public/js/navigation-ui.js`));
-    app.get('/textarea-control', (req, res) => res.sendFile(`${__dirname}/public/js/textarea-control.js`));
-    app.get('/unloading', (req, res) => res.sendFile(`${__dirname}/public/js/unloading.js`));
-    app.get('/getlist', async (req, res) => res.jsonp(await translationControl.COUNTRY_LIST_EN));
-    app.get('/translation', async (req, res) => res.jsonp(await translationControl.sendTranslationBack(req.query.text)));
-    app.get('/close', async (req, res) => res.jsonp(await translationControl.close()));
+    app.get(`${configuration.file.client.req.listControl}`, (req, res) => res.sendFile(`${__dirname}${configuration.file.server.res.listControl}`));
+    app.get(`${configuration.file.client.req.navigationUi}`, (req, res) => res.sendFile(`${__dirname}${configuration.file.server.res.navigationUi}`));
+    app.get(`${configuration.file.client.req.textareaControl}`, (req, res) => res.sendFile(`${__dirname}${configuration.file.server.res.textareaControl}`));
+    app.get(`${configuration.file.client.req.unloading}`, (req, res) => res.sendFile(`${__dirname}${configuration.file.server.res.unloading}`));
+    app.get(`${configuration.file.client.req.getlist}`, async (req, res) => res.jsonp(await translationControl.COUNTRY_LIST_EN));
+    app.get(`${configuration.file.client.req.translationUrl}`, async (req, res) => res.jsonp(await translationControl.sendTranslationBack(req.query.text)));
+    app.get(`${configuration.file.client.req.close}`, async (req, res) => res.jsonp(await translationControl.close()));
 
     return this;
 })();
