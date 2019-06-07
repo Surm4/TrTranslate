87067824fbee0b5aee7a03a6249509c99609d398
SurmaAa <anondebilwkazdymfredzie@gmail.com>
2019-06-07 23:46:46 +0200

    making list work... continued
    todo: add variable with functions to prepare workshop functions because we have no access in pageevaluate

--- translation.js
+++ translation.js
@@ -2,6 +2,7 @@ const TranslationControl = (() => {
     const configuration = require("./public/config/config.json");
     const puppeteer = require('puppeteer');
     
+    //Arrays of languages/languages functions
     const changeSourceLanguageFunctions = configuration.codeUtils.fromLangFunctions.split("|");
     const changeTranslationLanguageFunctions =  configuration.codeUtils.toLangFunctions.split("|");
     const COUNTRY_LIST_PL = configuration.countryList.pl.split(",");
@@ -14,6 +15,10 @@ const TranslationControl = (() => {
     let translationDocumentElement = ".translation";
     const translationWaiterElement = ".translating";
     const translationWaiterElement2 = ".empty";
+    const moreFromLangButton = ".sl-more";
+    const fromLangListBlock = ".language-list-unfiltered-langs-sl_list";
+    const moreToLangButton = ".tl-more";
+    const toLangListBlock = ".language-list-unfiltered-langs-tl_list";
     const googleCharactersLimit = 5000;
     let translatedDocumentArray = [];
     let browserBusy = false;
@@ -23,11 +28,12 @@ const TranslationControl = (() => {
 
     const sendTranslationBack = async (documentToTranslate, ...langs) => {
         if (browserBusy) return configuration.msg.dev.code.alreadyOpened;
-
+        const fromLangFunction = changeSourceLanguageFunctions[langs[0]];
+        const toLangFunction = changeTranslationLanguageFunctions[langs[1]];
         browserBusy = true;
-        translatedDocumentArray = [];
+        translatedDocumentArray = []; //clears latest translation
         await anyBrowserOpened();
-        const translationDone = await translateDocument(documentToTranslate, page, browser);
+        const translationDone = await translateDocument(documentToTranslate, page, browser, fromLangFunction, toLangFunction);
         browserBusy = false;
         
         return translationDone;
@@ -58,9 +64,9 @@ const TranslationControl = (() => {
         };
     };
 
-    const translateDocument = async (documentToTranslate, page) => {
+    const translateDocument = async (documentToTranslate, page, fromLangFunction, toLangFunction) => {
         const documentToTranslateFragments = documentToTranslate.match(new RegExp(`.{1,${googleCharactersLimit}}`, "g"));
-        await automate(documentToTranslateFragments, page);
+        await automate(documentToTranslateFragments, page, fromLangFunction, toLangFunction);
         return stringJoin(translatedDocumentArray);
     };
 
@@ -68,7 +74,19 @@ const TranslationControl = (() => {
         return translatedDocumentArray.join(" ");
     };
 
-    const automate = async (documentToTranslateFragments, page) => {
+    const setLanguages = async (page) => {
+        await page.waitForSelector(moreFromLangButton);
+        await page.click(moreFromLangButton);
+        await page.waitFor(fromLangListBlock);
+        await page.evaluate((fromLangFunction) => eval(fromLangFunction)); // TODO: !IMPORTANT THERE SHOULD BE functions embeded IN PREPARE WORKSHOP
+        await page.click(moreToLangButton);
+        await page.waitFor(toLangListBlock);
+        await page.evaluate((t) => {eval(toLangFunction)});
+        return configuration.msg.dev.code.OK_STATUS;
+    };
+
+    const automate = async (documentToTranslateFragments, page, fromLangFunction, toLangFunction) => {
+        await setLanguages(page);
         for (let documentToTranslateFragment of documentToTranslateFragments) {
             /* Write into input */
             await page.evaluate((sourceDocumentElement, sourceDocumentElementOrigin, documentToTranslateFragment) => {
