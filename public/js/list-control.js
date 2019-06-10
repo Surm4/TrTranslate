770cfadacbdec4cc2307a5fb862f3977d83c008a
SurmaAa <anondebilwkazdymfredzie@gmail.com>
2019-06-10 23:30:56 +0200

    removing unnecessary log

--- public/js/list-control.js
+++ public/js/list-control.js
@@ -9,7 +9,6 @@ const ListControl = async function() {
 
     const handleLanguageChange = (lang, langGoal) => {
         const langIndex = COUNTRY_LIST_EN.findIndex(el => el === lang);
-        console.log(langIndex)
         localStorage.setItem(langGoal, langIndex);
     };
 
