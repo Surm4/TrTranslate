24c1426ab8a8adf62455fda69c6cdf76e2ccd1f4
SurmaAa <anondebilwkazdymfredzie@gmail.com>
2019-06-15 16:13:06 +0200

    unused file

--- public/js/navigation.js
+++
@@ -1,7 +0,0 @@
-const navItems = document.querySelector(".ms-nav");
-navItems.addEventListener("click", (e) => { handleNavClick(e); });
-
-const handleNavClick = (e) => {
-    const el = e.target;
-    el.classList.toggle("ms-nav-active");
-}
