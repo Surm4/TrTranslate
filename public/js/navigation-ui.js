e508820a1c66416e6c0616b2f1117d3b5a44eb10
SurmaAa <anondebilwkazdymfredzie@gmail.com>
2019-06-15 16:12:05 +0200

    working tabs

--- public/js/navigation-ui.js
+++ public/js/navigation-ui.js
@@ -1,14 +1,25 @@
 const NavigationControl = () => {
+    const navSection = document.querySelectorAll(".ms-translation-container");
     const navItemsContainer = document.querySelector(".ms-nav");
     const navItems = [...document.querySelectorAll(".ms-nav .col-sm")];
     navItemsContainer.addEventListener("click", (e) => { handleNavClick(e); });
     
     const handleNavClick = (e) => {
         const el = e.target;
+        const lvl = el.dataset.lvl;
         if (!el.classList.contains("ms-nav-active")) {
             el.classList.add("ms-nav-active");
+            // navSection[lvl].classList.remove("ms-display-none");
+            toggleSection(lvl);
             navItems.forEach(item => removeOtherItemsStyles(el, item));
+            
         }
+    };
+
+    const toggleSection = (lvl) => {
+        const keysToHide = Object.keys(navSection).filter(el => el != lvl);
+        navSection[lvl].classList.remove("ms-display-none");
+        keysToHide.forEach(el => navSection[el].classList.add("ms-display-none"));
     };
 
     const removeOtherItemsStyles = (el, item) => {
